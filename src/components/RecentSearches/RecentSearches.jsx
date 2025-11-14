eb49f3abbb0d7c55e09b48865913a8d6bc1b90ea
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-11-14 17:00:32 +0200

    almost done header? started doing log in and sign up modal

---
+++ src/components/RecentSearches/RecentSearches.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { Container } from "../Container/Container";
+
+export const RecentSearches = ({q}) => {
+    return (
+        <section>
+            <Container>
+                <div>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+            </Container>
+        </section>
+    )
+}
