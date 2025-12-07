f7c891e906c3288149c05d5598b34417d8563344
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-12-07 14:14:33 +0200

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,7 +18,7 @@ export const Footer = () => {
           <div className={styles.footer__wrap}>
             <div className={styles.footer__subwrap}>
               <h2 className={styles.footer__title}>Address</h2>
-              <a href="#" className={styles.footer__link}>
+              <a href="https://maps.app.goo.gl/azMYyCBvhG8EMtrNA" className={styles.footer__link}>
                 Svobody str. 35 Kyiv <br /> Ukraine
               </a>
             </div>
@@ -26,18 +26,18 @@ export const Footer = () => {
               <h2 className={styles.footer__title}>Contact us</h2>
               <ul className={styles.footer__list}>
                 <li>
-                  <a href="#" className={styles.footer__sm}>
+                  <a href="https://www.instagram.com/forecast_platform?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className={styles.footer__sm}>
                     <img src={Inst} alt="inst" />
                   </a>
                 </li>
                 <li>
-                  <a href="#" className={styles.footer__sm}>
+                  <a href="https://www.facebook.com/wxforecast/" className={styles.footer__sm}>
                     <img src={Fb} alt="facebook" />
                   </a>
                 </li>
                 <li>
-                  <a href="#" className={styles.footer__sm}>
-                    <img src={Ws} alt="whatsapp" />
+                  <a href="https://www.tiktok.com/@weather?is_from_webapp=1&sender_device=pc" className={styles.footer__sm}>
+                    <img src={Ws} alt="tt" />
                   </a>
                 </li>
               </ul>
--- src/components/Header/MobileMenu/MobileMenu.jsx
+++ src/components/Header/MobileMenu/MobileMenu.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useAuth } from "../../../context/AuthContext";
 
 import { Container } from "../../Container/Container";
@@ -6,6 +7,19 @@ import styles from "./MobileMenu.module.scss";
 import User from "../../../images/user.svg";
 export const MobileMenu = ({ isMenuOpen, onClose, onClick }) => {
   const { user } = useAuth();
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, [isMenuOpen]);
+
   return (
     <>
       {isMenuOpen && <div className={styles.backdrop} onClick={onClose}></div>}
